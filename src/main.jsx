7150286c3af8a49225ba2b6d2da9bebbb5a2eae8
Highscoretech <highscoretechng@gmail.com>
2025-04-22 22:49:22 +0100

    code congatinate provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { AppContextProvider } from './context/AppContext.jsx';
-// import { BrowserRouter } from 'react-router';
+import { AppContextProvider } from './context/AppContext.jsx';
+import { BrowserRouter } from 'react-router';
 
 
 createRoot(document.getElementById('root')).render(
-  // <BrowserRouter>
-  //   <AppContextProvider>
-  //     <App />
-  //   </AppContextProvider>
-  // </BrowserRouter>
-  <App />
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 )
