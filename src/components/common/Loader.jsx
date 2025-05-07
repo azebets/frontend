8dafaabbb80a44557ae5063ea92593d9fdbc919b
Highscoretech <highscoretechng@gmail.com>
2025-05-07 07:30:51 +0100

    balance display

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center w-full justify-center min-h-screen">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-opacity-75"></div>
     </div>
   );
